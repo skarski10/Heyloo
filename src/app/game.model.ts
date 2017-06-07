5cac9ee284f6868d196cf595b381c86dde246cf7
Kory Skarbek <koryskarbek@yahoo.com>
2017-06-07 12:53:05 -0700

    add player and question models

--- src/app/game.model.ts
+++ src/app/game.model.ts
@@ -1,3 +1,5 @@
+import { Player } from './player.model';
+import { Question } from './question.model';
 export class Game {
   current_question: number
   constructor(public id: number, public game_state: string, public game_over: boolean, public player_list: Player[], public question_list: Question[]){
--- src/app/host.service.ts
+++ src/app/host.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Game } from './game.model';
+import { Player } from './player.model';
+import { Question } from './question.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireAuth } from 'angularfire2/auth';
