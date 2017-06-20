cd026316c916ce8a683f98a3fbc23a2953e368bd
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-06-20 12:45:17 -0700

    fix more merge conflicts

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -16,14 +16,11 @@ import { Question } from '../question.model';
 export class StudentComponent implements OnInit, DoCheck {
   currentGame: FirebaseObjectObservable<any[]>;
   currentStudent: FirebaseObjectObservable<any[]>;
-<<<<<<< HEAD
   currentQuestion: Question[];
   startTime;
-=======
   questions: Question[];
   currentQuestion;
   currentQuestionIndex: number = 0;
->>>>>>> 481a98ee03ee6479c4571daa925c24b70615463a
 
   constructor(private route: ActivatedRoute, private studentService: StudentService, private router: Router, private hostService: HostService) { }
 
@@ -41,12 +38,10 @@ export class StudentComponent implements OnInit, DoCheck {
       currenGameQuestion = data['current_question'];
     })
     this.currentStudent = this.studentService.getStudentGameKeyAndId(currentGameKey, studentId);
-<<<<<<< HEAD
     this.currentGame.subscribe(data => {
       // console.log(data);
       this.currentQuestion = data["question_list"][data["current_question"]]
     })
-=======
     this.questions = this.hostService.getQuestions();
     this.currentQuestion = this.hostService.getQuestionKeyAndId(currentGameKey, currenGameQuestion)
   }
@@ -58,7 +53,6 @@ export class StudentComponent implements OnInit, DoCheck {
     else{
       this.studentService.editStudentPoints(this.currentStudent, this.currentGame, false);
     }
->>>>>>> 481a98ee03ee6479c4571daa925c24b70615463a
   }
 
   ngDoCheck(){
@@ -75,19 +69,5 @@ export class StudentComponent implements OnInit, DoCheck {
 
 }
 
-<<<<<<< HEAD
-=======
-
-// this.route.params.forEach((urlParameters) => {
-//   this.studentId = urlParameters['id'];
-//     });
-//      this.studentService.getStudent(this.studentId).subscribe(dataLastEmittedFromObserver => {
-//     //  this.studentToDisplay = new Player
-//      (dataLastEmittedFromObserver.name,
-//      dataLastEmittedFromObserver.correct,
-//      dataLastEmittedFromObserver.wrong)
-//    })
-//    this.students = this.studentService.getStudents();
->>>>>>> 481a98ee03ee6479c4571daa925c24b70615463a
 
 //https://stackoverflow.com/questions/36320821/passing-multiple-route-params-in-angular2     HOW TO USE MULTIPLE ROUTER PARAMS
